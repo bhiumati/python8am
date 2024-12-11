ec1c4526162aae3a1555ecb14575d3ed4983cc39
bhiumati <bhiumatibk0@gmail.com>
2024-12-11 09:27:30 +0545

    update

---
+++ main.py
@@ -0,0 +1,76 @@
+# print("Hello python")
+
+# name ='ram'
+# age =20
+
+# print("My name is ",name)
+# print("My age is ",age)
+
+# print(f"My name is {name} age {age}")
+# print(f"my name  is bhimu age 21")
+
+
+# full_name ="Ram Abc"
+
+# print(f"My name is {full_name}")
+
+# x=5
+# y=10
+
+# print(x+y)
+# name,email,phone,address
+# name =input("Enter your name: ")
+
+# print(f"My name is {name}")
+# name='bhimu'
+# email='bhiumatibk@gmail.com'
+# phone='9860275012'
+# address='siva chok'
+# print(f"my name is{name}  email{email} phone{phone} address{address}")
+
+
+# name =input("Enter your name: ")
+# email =input("Enter your email: ")
+
+# print(f"My name is {name} email address {email}")
+
+# name =input("Enter your name:")
+# email =input("Enter your email:")
+# phone =input("Enter your phone:")
+# address =input("Enter your address:")
+# age =input("Enter your age:")
+
+# print(f"my name is {name} emailaddress{email} phone{phone} address{address} age{age}")
+
+# data types: int,float,complex
+# list, tuple, set, dictionary
+
+# a =45.6
+# print(type(a))
+
+# a =int(input("Enter a: "))
+# b= int(input("Enter b: "))
+# print(a+b)
+
+# Write a program to enter five subject mark and calculate total percentage
+# 
+
+# english =int(input("Enter english: "))
+# match =int(input("Enter match: "))
+# social =int(input("Enter social: "))
+# science =int(input("Enter science: "))
+# health =int(input("Enter helth: "))
+
+# total =english+match+social+science+health
+# per =total/5
+
+# print("Total",total)
+# print("Percentage: ",per)
+
+# p =int(input("enter p:"))
+# t=int(input("enter t:"))
+# r=int(input("enter r:"))
+# total =p+t+r/100
+
+# print("total ",total)
+
---
+++ practice.py
@@ -0,0 +1,60 @@
+# name ='ram'
+# age =345
+
+# print(f"my name is {name}  age {age}")
+
+# full_name=input("Enter full name: ")
+# print(f"my name is {full_name}")
+
+
+# x = int(input("Enter x: "))
+# y =int(input("Enter y: "))
+
+# print(x+y)
+
+
+
+
+# a =int(input("enter a:"))
+# b =int(input("enter b:"))
+# c =int (input("enter c"))
+# d =int(input("enter d:"))
+
+
+# name ='bhimu'
+# age =21
+# print(f"my name is {name} age {age}")
+
+
+# full_name=input("enter full_name:")
+# print(f"my name is {full_name})
+
+
+
+
+
+
+
+# a =int(input("enter a:"))
+# b =int(input("enter b:"))
+# c =int(input("enter c:"))
+# d =int(input("enter d:"))
+# e =int(input("enter e:"))
+# print(a+b+c+d+e)
+
+
+# full_name=input("enter full_name :")
+# print(f"my name is {full_name}")
+
+
+# write a program to enter five subject mark and calculate total percentage
+
+# web =int(input("enter web:"))
+# html =int(input("enter html:"))
+# css =int(input("enter css:"))
+# java =int(input("enter java:"))
+# pythan =int(input("enter pythan:"))
+# Total=web+html+css+java+pythan
+# per =topal1/5 
+# print =("total" ,total)
+# print = ("percentage: per")
